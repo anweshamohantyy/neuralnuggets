59fe0fd7b2d134d471317f470d644f23c0812d67
Beverly Phu <beverlyphu@gmail.com>
2025-05-19 10:49:31 -0700

    fixed a few errors with kaggle and keras

--- Neural Nuggets.py
+++ Neural Nuggets.py
@@ -1,4 +1,4 @@
-# Importing Libraries 
+# Importing Libraries
 import pandas as pd
 import numpy as np
 import os
@@ -6,68 +6,60 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
-import keras
-import kaggle
 from IPython.display import Audio
 import warnings
 warnings.filterwarnings('ignore')
 
 # Kaggle authentication
-os.system("mkdir ~/.kaggle")
-os.system("touch ~/.kaggle/kaggle.json")
-api_token = {"username":"sanjanapremkumar","key":"e2acda3c12ec484d98b029a64286a8c6"}
-import json
-with open('/root/.kaggle/kaggle.json', 'w') as file:
-    json.dump(api_token, file)
-os.system("chmod 600 ~/.kaggle/kaggle.json")
+os.makedirs(os.path.expanduser("~/.kaggle"), exist_ok=True)
 
 # Download TESS dataset
 os.system("kaggle datasets download -d ejlok1/toronto-emotional-speech-set-tess")
-os.system("unzip /content/toronto-emotional-speech-set-tess.zip")
-Tess = r"/content/TESS Toronto emotional speech set data"
+os.system("unzip toronto-emotional-speech-set-tess.zip")
+Tess = os.path.join(os.getcwd(), "TESS Toronto emotional speech set data")
 
 # Sort the emotions in the dataset
 emotion = []
 path = []
 tess_directory_list = os.listdir(Tess)
 for dir in tess_directory_list:
-    directories = os.listdir(Tess +'/'+ dir)
+    directories = os.listdir(os.path.join(Tess, dir))
     for file in directories:
-        part = file.split('.')[0]                   #Splitting the file whenever it sees a period (ex: removing .wav)
-        part = part.split('_')[2]                   #Splitting after second underscore to clasify the emotion 
+        part = file.split('.')[0]                   # Splitting the file whenever it sees a period (ex: removing .wav)
+        part = part.split('_')[2]                   # Splitting after second underscore to classify the emotion
         if part=='ps':
-            emotion.append('surprise')     #label ps as suprise
+            emotion.append('surprise')              # label ps as surprise
         else:
             emotion.append(part)
-        path.append(Tess + dir + '/' + file)
-        
+        path.append(os.path.join(Tess, dir, file))  # use os.path.join for correct file paths
+
 # Create a dataframe
 tess_df = pd.DataFrame()
-tess_df["path"] = path        #creating a column called path
-tess_df["emotion"] = emotion          #creating a column called emotion
+tess_df["path"] = path        # creating a column called path
+tess_df["emotion"] = emotion  # creating a column called emotion
 tess_df.head()
 
 # Create the waveplot function to display the amplitude of an audio signal
 def waveplot(data, sr, emotion):
-    plt.figure(figsize=(10,4))              #Creates chart size
-    plt.title(emotion, size=20)             #Sets title of plot to the specified emotion   
-    librosa.display.waveshow(data, sr=sr, color='pink')     #Adds the waveform to a plot
-    plt.show()      #Renders the plot
+    plt.figure(figsize=(10,4))
+    plt.title(emotion, size=20)
+    librosa.display.waveshow(data, sr=sr, color='pink')
+    plt.show()
 
 # Create the spectogram function to display the frequency and amplitude over time
 def spectogram(data, sr, emotion):
-    x = librosa.stft(data)          #transforms data into time-frequency domain for spectogram use
-    xdb = librosa.amplitude_to_db(abs(x))   #transforms data to decibells(dB)
-    plt.figure(figsize=(11,4))              #Sets the size of the chart
-    plt.title(emotion, size=20)             #Sets the title of the plot to the specified emotion
-    librosa.display.specshow(xdb, sr=sr, x_axis='time', y_axis='hz', cmap = 'spring')   #Makes 2D array of decibel-scaled amplitudes
-    plt.colorbar()      #Adds a colorbar: Mapping between color intensity and dB values
-    plt.show()      #Renders the plot
-       
+    x = librosa.stft(data)
+    xdb = librosa.amplitude_to_db(abs(x))
+    plt.figure(figsize=(11,4))
+    plt.title(emotion, size=20)
+    librosa.display.specshow(xdb, sr=sr, x_axis='time', y_axis='hz', cmap='spring')
+    plt.colorbar()
+    plt.show()
+
 # Emotion Sample (Fear)
 emotion = 'fear'
-path = np.array(tess_df["path"][tess_df["emotion"] == emotion])[0]   # line gives you the file path to one 'fear' audio sample
-data, sr = librosa.load(path)   #Loads the audio signal so it can be visualized or used in processing.
+path = np.array(tess_df["path"][tess_df["emotion"] == emotion])[0]
+data, sr = librosa.load(path)
 waveplot(data, sr, emotion)
 spectogram(data, sr, emotion)
 Audio(path)
@@ -122,42 +114,39 @@ Audio(path)
 
 # Feature extraction
 def extract_mfcc(filename):
-    y, sr = librosa.load(filename, duration=3, offset=0.5)      #Loads 3 seconds of audio, starting at 0.5 seconds
-    mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)      #Turns audio into numbers for machine to learn
+    y, sr = librosa.load(filename, duration=3, offset=0.5)
+    mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
     return mfcc
-extract_mfcc(tess_df["path"][0])    #applies the fuction to the first audio file
+extract_mfcc(tess_df["path"][0])
 
 # Processes every file in the 'path' column and creates a list of MFCC features for all
-X_mfcc = tess_df["path"].apply (lambda x: extract_mfcc(x))      #Applies the extract function to every file path
-X_mfcc
-
-X = [x for x in X_mfcc]     #Loops through all MFCC and converts into a regular python list 
-X = np.array(X)         #Converts the list into a 2d num array
-X.shape             
-
-X = np.expand_dims(X, -1)       #Adds a new dimension to make the shape (2800, 40, 1)
+X_mfcc = tess_df["path"].apply(lambda x: extract_mfcc(x))
+X = [x for x in X_mfcc]
+X = np.array(X)
+X.shape
+X = np.expand_dims(X, -1)
 X.shape
 
 # Converts categorical labels into binary vectors
 from sklearn.preprocessing import OneHotEncoder
-encoder = OneHotEncoder()  # Each emotion is turned into a vector of 0s and 1s
-y = encoder.fit_transform(tess_df[['path']])        #Changes the emotion labels into understandable numbers for model to read
+encoder = OneHotEncoder()
+y = encoder.fit_transform(tess_df[['emotion']])        # Changes the emotion labels into understandable numbers for model to read
 y = y.toarray()
 y.shape
 
 # Create Long Short-Term Model
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
 
 # Creates neural network model where layers are stacked in order
 model = Sequential([
-    LSTM(256, return_sequences=False, input_shape=(40,1)),      #256 memory units, gives final summary only
-    Dropout(0.2),       #Ignores 20% of memory to prevent overfitting (memorizing data)
-    Dense(128, activation='relu'),  #Reduces neurons to 128 (helps model focus on key characteristics). Uses ReLU function (kills weak signals [negative values])
+    LSTM(256, return_sequences=False, input_shape=(40,1)),
     Dropout(0.2),
-    Dense(64, activation='relu'),   #Reduces neurons to 64. Uses ReLU function (kills weak signals [negative values])
-    Dropout(0.2),       
-    Dense(7, activation='softmax')  #Final layer: 7 emotions. Uses Softmax to convert outputs to probabilities that add up to 100%
+    Dense(128, activation='relu'),
+    Dropout(0.2),
+    Dense(64, activation='relu'),
+    Dropout(0.2),
+    Dense(7, activation='softmax')
 ])
 
 # Loss = Measures how wrong predictions are. Optimizer = Tries to correct mistakes. Metrics = % of correct predictions
@@ -165,30 +154,30 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 # Prints a summary of model with Layer Types, Output Shapes, and Param #
 model.summary()
 
-# Trains the model for 50 rounds by checking the accuracy using 20% of the data 
-history = model.fit(X, y, validation_split=0.2, epochs=50, batch_size=64)  
+# Trains the model for 50 rounds by checking the accuracy using 20% of the data
+history = model.fit(X, y, validation_split=0.2, epochs=50, batch_size=64)
 
-#Training accuracy and validation accuracy, seeing model's performance
-epochs = list(range(50))  # Sets up the x-axis values for the accuracy plot
-acc = history.history['accuracy'] #stores the training accuracy so it can be plotted in a graph
-val_acc = history.history['val_accuracy'] #stores the validation accuracy so it can be plotted in a graph
+# Training accuracy and validation accuracy, seeing model's performance
+epochs = list(range(50))
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 
 # Plot the Results
 plt.title('Training vs Validation Accuracy')
 plt.plot(epochs, acc, label='train accuracy', color='orchid')
 plt.plot(epochs, val_acc, label='val accuracy', color='lightblue')
-plt.xlabel('epochs')    #Adds axis labels
+plt.xlabel('epochs')
 plt.ylabel('accuracy')
-plt.legend()            #Adds a legend 
-plt.show()              #Displays plot
+plt.legend()
+plt.show()
 
-#Training and validation loss which measure how wrong the models predictions are
+# Training and validation loss which measure how wrong the models predictions are
 # Visualize loss changing over time
 loss = history.history['loss']
 val_loss = history.history['val_loss']
-plt.plot(epochs, loss, label='train loss')      #Plots two lines: training loss and validation loss against the number of epochs
-plt.plot(epochs, val_loss, label='val loss')       #epochs is a list or array representing the numbers
-plt.xlabel('epochs')        #Adds axis labels
-plt.ylabel('loss')         
-plt.legend()                #Adds a legend 
-plt.show()                  #Displays plot
+plt.plot(epochs, loss, label='train loss')
+plt.plot(epochs, val_loss, label='val loss')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
